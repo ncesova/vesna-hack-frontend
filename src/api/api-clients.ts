9906d673b6000106361e8e54c60fc750c34afa96
Mikkoplus <sharov.012@gmail.com>
2025-04-06 18:49:51 +0300

    refresh token ngork

--- src/api/api-clients.ts
+++ src/api/api-clients.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://8bbd-89-22-233-120.ngrok-free.app/api/v1";
+const API_URL = "https://891a-89-22-233-120.ngrok-free.app/api/v1";
 
 export const $api = axios.create({
   baseURL: API_URL,
