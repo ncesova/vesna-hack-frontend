9321308c66b5b0f958d76416e952e1dc3306b5ce
Sova <108209320+ncesova@users.noreply.github.com>
2025-04-06 13:20:10 +0300

    asfddsaf

--- src/api/api-clients.ts
+++ src/api/api-clients.ts
@@ -5,7 +5,8 @@ const API_URL = "https://8bbd-89-22-233-120.ngrok-free.app/api/v1";
 export const $api = axios.create({
   baseURL: API_URL,
   headers: {
+    "ngrok-skip-browser-warning": "69420",
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  // withCredentials: true,
 });
